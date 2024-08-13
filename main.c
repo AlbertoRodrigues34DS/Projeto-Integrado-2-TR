8310fc4019fe15b2364ca7894306a19c9baf655b
AlbertoRodrigues34DS <albertofernandesrodrigues8@gmail.com>
2024-08-13 11:08:52 -0300

    Código desenvolvido
    Continuar por esse

--- main.c
+++ main.c
@@ -19,9 +19,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdint.h>
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include"ST7735\ST7735.h"
 
 /* USER CODE END Includes */
 
@@ -32,6 +34,21 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define BOTAO9 HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_9)
+#define BOTAO10 HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_10)
+#define BOTAO11 HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_11)
+#define BOTAO12 HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_12)
+
+#define LED3_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,1)
+#define LED4_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,1)
+#define LED5_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,1)
+#define LED6_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,1)
+
+#define LED3_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,0)
+#define LED4_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,0)
+#define LED5_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,0)
+#define LED6_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,0)
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,16 +57,27 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
-int camp[3][3];
+short jogar=0;
+uint16_t cor=BLUE;
+unsigned int camp[6][6];
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
-
+// short está sendo utilizado por causa do espaço ocupado que é menor do que int
+short Menu( uint16_t cor);
+short Jogo(void);
+short DificuldadeLed(uint16_t cor);
+short Derrota(short VMenu);//V=voltar
+short Vitoria(short VMenu);//V=voltar
+short MudarCor(uint16_t cor);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -85,26 +113,24 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-for(int i=0; i<3;i++){
-	for(int j=0; j<3;j++){
-		int x=rand()%2;
-        camp[i][j]=x;
-	}
-}
+  ST7735_Init();
+ // unsigned int C = sizeof(camp[0]); //C=Coluna
+ // unsigned int L = sizeof(camp)/C; //L=Linha
+ // srand(SysTick->VAL);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  int l=rand()%3;
-	  int a=rand()%3;
-	  if(camp[l][a]==1){
-		  do{
+	 // short cor=0;
+	   Menu(cor);
+	   ST7735_FillScreen(BLACK);
+	   while (1){
 
-		  }while(1);
-	  }
+	   }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -148,19 +174,205 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ST7735_CS_GPIO_Port, ST7735_CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, ST7735_DC_Pin|ST7735_RES_Pin|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : ST7735_CS_Pin */
+  GPIO_InitStruct.Pin = ST7735_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ST7735_CS_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ST7735_DC_Pin ST7735_RES_Pin PB3 PB4
+                           PB5 PB6 */
+  GPIO_InitStruct.Pin = ST7735_DC_Pin|ST7735_RES_Pin|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA9 PA10 PA11 PA12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
+short Menu( uint16_t cor){
+	ST7735_FillScreen(cor);
+	int escolha=0;
+	ST7735_WriteString(13, 0,"Campo Minado",Font_11x18, BLACK, BLUE );
+	ST7735_WriteString(30, 30,"P9-Jogar!",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 45,"P10-Dificuldade:",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 60,"P11-Cores:",Font_7x10, BLACK, BLUE );
+
+
+	while(escolha==0){
+    	if (BOTAO9 == 0 && BOTAO10 == 1 && BOTAO11 == 1 && BOTAO12 == 1) {
+    	escolha=1;
+    	}
+    	else if (BOTAO9 == 1 && BOTAO10 == 0 && BOTAO11 == 1 && BOTAO12 == 1) {
+    	    	    escolha=2;
+    	    	  }
+
+	else if (BOTAO9 == 1 && BOTAO10 == 1 && BOTAO11 == 0 && BOTAO12 == 1) {
+	    	    	    escolha=3;
+	    	    	  }
+		}
+
+	switch (escolha) {
+			case 1:
+		    Jogo();
+			break;
+
+			case 2:
+			DificuldadeLed(cor);
+			break;
+
+			case 3:
+			MudarCor(cor);
+			Menu(cor);
+			break;
+		}
+
+
+	}
+
+short Jogo(void){
+//int bomba= rand() % 1 (camp[6][6]);
+
+}
+short MudarCor(uint16_t cor)
+{
+	ST7735_FillScreen(cor);
+		short escolhacor=0;
+		ST7735_WriteString(13, 0, "Tabela de cores",Font_7x10, BLACK, BLUE );
+		ST7735_WriteString(30, 15, "P9-Verde",Font_7x10, BLACK, BLUE );
+		ST7735_WriteString(30, 30, "PA10-Magenta",Font_7x10, BLACK, BLUE );
+		ST7735_WriteString(30, 60, "P11-Azul",Font_7x10, BLACK, BLUE );
+		ST7735_WriteString(30, 45, "P12-Voltar",Font_7x10, BLACK, BLUE );
+
+		while(escolhacor==0){
+
+
+			if (BOTAO9 == 0 && BOTAO10 == 1 && BOTAO11 == 1 && BOTAO12 == 1) {
+				cor=GREEN;
+				ST7735_FillScreenFast(cor);
+				ST7735_WriteString(13, 0, "Tabela de cores",Font_7x10, BLACK, BLUE );
+				ST7735_WriteString(30, 15, "P9-Verde",Font_7x10, BLACK, BLUE );
+				ST7735_WriteString(30, 30, "PA10-Magenta",Font_7x10, BLACK, BLUE );
+				ST7735_WriteString(30, 45, "P11-Azul",Font_7x10, BLACK, BLUE );
+				ST7735_WriteString(30, 60, "P12-Voltar",Font_7x10, BLACK, BLUE );
+
+			    	}
+			    	else if (BOTAO9 == 1 && BOTAO10 == 0 && BOTAO11 == 1 && BOTAO12 == 1) {
+			    	    cor=MAGENTA;
+			    	    ST7735_FillScreenFast(cor);
+			    	    ST7735_WriteString(13, 0, "Tabela de cores",Font_7x10, BLACK, BLUE );
+						ST7735_WriteString(30, 15, "P9-Verde",Font_7x10, BLACK, BLUE );
+		  				ST7735_WriteString(30, 30, "PA10-Magenta",Font_7x10, BLACK, BLUE );
+ 	    				ST7735_WriteString(30, 45, "P11-Azul",Font_7x10, BLACK, BLUE );
+	    				ST7735_WriteString(30, 60, "P12-Voltar",Font_7x10, BLACK, BLUE );
+			    	    	  }
+
+				else if (BOTAO9 == 1 && BOTAO10 == 1 && BOTAO11 == 0 && BOTAO12 == 1) {
+					cor=BLUE;
+					ST7735_FillScreenFast(cor);
+					ST7735_WriteString(13, 0, "Tabela de cores",Font_7x10, BLACK, BLUE );
+					ST7735_WriteString(30, 15, "P9-Verde",Font_7x10, BLACK, BLUE );
+			  		ST7735_WriteString(30, 30, "PA10-Magenta",Font_7x10, BLACK, BLUE );
+					ST7735_WriteString(30, 45, "P11-Azul",Font_7x10, BLACK, BLUE );
+					ST7735_WriteString(30, 60, "P12-Voltar",Font_7x10, BLACK, BLUE );
+				    	    	  }
+				else if(BOTAO9 == 1 && BOTAO10 == 1 && BOTAO11 == 1 && BOTAO12 == 0){
+					Menu(cor);
+				}
+		}
+
+}
+short DificuldadeLed(uint16_t cor){
+	int escolhadificuldade=0;
+
+	ST7735_FillScreenFast(cor);
+	ST7735_WriteString(13, 0, "Escolha o nivel",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 15, "P9-Facil",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 30, "PA10-Medio",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 45, "P11-Dficil",Font_7x10, BLACK, BLUE );
+	ST7735_WriteString(30, 60, "P12-Voltar",Font_7x10, BLACK, BLUE );
+
+	while(escolhadificuldade==0){
+	if(BOTAO9==0){
+	LED6_LIGA;
+}
+	else if(BOTAO10==0){
+		LED6_LIGA;
+		LED5_LIGA;
+	}
+	else if(BOTAO11==0){
+	LED6_LIGA;
+	LED5_LIGA;
+	LED4_LIGA;
+	}
+}
+}
 
 /* USER CODE END 4 */
 
